774b7f18f4ac8a93068ee87fe5072d978a8fa526
gustavorodi <37717445+gustavorodi@users.noreply.github.com>
2019-02-21 00:16:00 -0300

    Tem que arrumar o divisor

---
+++ Java/lista/Pedido.java
@@ -0,0 +1,24 @@
+public interface Pedido
+{
+    public static void main (String[] args)
+    {
+
+      
+        if(args.length==0)
+        {
+            System.out.println("deu erro");
+            System.exit(0);
+        }
+
+        Integer enviado1 = Integer.valueOf(args[0]);
+        Integer enviado2 = Integer.valueOf(args[1]);
+        
+ 	    System.out.println("Soma:"              +           (enviado1 + enviado2));
+        System.out.println("Subtracao:"         +           (enviado1 - enviado2));
+        System.out.println("Multiplicao:"       +           (enviado1 * enviado2));
+        System.out.println("Divisao:"           +(double)   (enviado1 % enviado2));
+        System.out.println("O maior numero :"   + Math.max  (enviado1 , enviado2));
+
+    }
+}
+
