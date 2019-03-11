ae6685ae7cd540b16907d63fe3e007e567e39b67
gustavorodi <37717445+gustavorodi@users.noreply.github.com>
2019-03-11 19:06:11 -0300

    Add files via upload

--- Java/lista/Geometria/GeometriaTeste.java
+++ Java/lista/Geometria/GeometriaTeste.java
@@ -1,15 +1,19 @@
 public class GeometriaTeste {
-    public static void main(String[] args) {
-        Ponto p1 = new Ponto(3.0,5.0);
-        Ponto p2 = new Ponto(5.0,5.0);
-        Ponto p3 = new Ponto(3.0,3.0);
-        Ponto p4 = new Ponto(5.0,3.0);
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
+    public static void main(String[] args) {
+
+        Quadrado quadrado = new Quadrado();
+        Trapezio trapezio = new Trapezio();
+        Retangulo retangulo = new Retangulo();
+        Quadrilatero quadrilatero = new Quadrilatero();
+        Paralelogramo paralelogramo = new Paralelogramo();
+        
+
+        System.out.println(new Quadrilatero() );
+        System.out.println(new Trapezio()     );
+        System.out.println(new Paralelogramo());
+        System.out.println(new Retangulo()    );
+        System.out.println(new Quadrado()     );
 
     }
-
 }
